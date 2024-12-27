55cbf2479e25c110849e07ceb2cc8ccdae07f35c
ZakiMohammed <shaihzaki67@gmail.com>
2024-12-28 00:35:31 +0300

    Ng Init: Upgraded to v19

--- ng-1-init/src/app/components/empty/empty.component.ts
+++ ng-1-init/src/app/components/empty/empty.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-empty',
-  templateUrl: './empty.component.html',
-  styleUrl: './empty.component.scss'
+    selector: 'app-empty',
+    templateUrl: './empty.component.html',
+    styleUrl: './empty.component.scss',
+    standalone: false
 })
 export class EmptyComponent {
 
